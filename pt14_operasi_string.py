982193e0533f3987707b7597af58edf08d1aaf45
Edria <alden.f.edria@gmail.com>
2023-02-28 21:49:58 +0700

    baru

--- pt14_operasi_string.py
+++ pt14_operasi_string.py
@@ -1,1 +1,17 @@
 # format string
+
+# contoh generic
+# string
+nama = "ucup"
+format_str = f"hello {nama}"
+print(format_str)
+
+# angka
+angka = 2005.5
+format_str = f"angka = {angka}"
+print(format_str)
+
+# booelan
+boolean = True
+format_str = f"angka = {angka}"
+print(format_str)
